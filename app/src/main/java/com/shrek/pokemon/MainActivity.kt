640ca6f2a2da435a056522b0bbc5adcdc820d703
Shreyak <shreyak@getmega.com>
2021-11-05 16:06:20 +0530

    [app] Refactored MainActivity2.kt -> MainActivity.kt. App run stable too.

--- app/src/main/java/com/shrek/pokemon/MainActivity.kt
+++ app/src/main/java/com/shrek/pokemon/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.shrek.pokemon
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.compose.rememberNavController
+import com.shrek.pokemon.ui.theme.MyApplicationTheme
+import com.shrek.pokemon.util.SetupNavGraph
 import com.shrek.pokemon.util.getInjector
 
+class MainActivity : ComponentActivity() {
 
-class MainActivity : AppCompatActivity() {
-
-
+    private val injector by lazy { getInjector(this) }
+    private val viewModel: MainViewModel by viewModels { injector.provideMainViewModel() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                val navHostController = rememberNavController()
+                SetupNavGraph(
+                    navHostController = navHostController,
+                    mainViewModel = viewModel,
+                )
+            }
+        }
     }
-
 }
+
