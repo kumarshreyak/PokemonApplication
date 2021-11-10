9701d65cf5c6d7f25d4bcbf13ba6507731ee6c23
Shreyak kumar <kumarshreyak@gmail.com>
2021-11-10 15:47:58 +0530

    removed unused code

--- pokemonLibrary/src/main/java/com/shrek/pokemonlibrary/client/PokemonClient.kt
+++ pokemonLibrary/src/main/java/com/shrek/pokemonlibrary/client/PokemonClient.kt
@@ -16,38 +16,6 @@ class PokemonClient internal constructor(
     private var logLevel: PokemonClientLogLevel
 ) {
 
-//    private val _pokemonShakespeareDescriptionSearchResult = MutableLiveData(PokemonApiResult<PokemonShakespeareDescription>())
-//    val pokemonShakespeareDescriptionSearchResult: LiveData<PokemonApiResult<PokemonShakespeareDescription>> = _pokemonShakespeareDescriptionSearchResult
-
-//    fun searchPokeShakespeareDescription(pokemonName: String) : PokemonApiResult<PokemonShakespeareDescription> {
-//        val apiResult = PokemonApiResult<PokemonShakespeareDescription>()
-//        _pokemonShakespeareDescriptionSearchResult.value = apiResult
-//        CoroutineScope(Dispatchers.IO).launch {
-//            fetchPokemonDescription(
-//                searchText = pokemonName,
-//                onError = { apiError ->
-//                    this.launch(context = Dispatchers.Main) {
-//                        apiResult.apply {
-//                            resultState = ResultState.ERROR
-//                            error = apiError?.toPokemonError()
-//                        }
-//                        _pokemonShakespeareDescriptionSearchResult.value = apiResult
-//                    }
-//                },
-//                onSuccess = { pokemon ->
-//                    this.launch(context = Dispatchers.Main) {
-//                        apiResult.apply {
-//                            resultState = ResultState.SUCCESS
-//                            result = pokemon
-//                        }
-//                        _pokemonShakespeareDescriptionSearchResult.value = apiResult
-//                    }
-//                },
-//            )
-//        }
-//        return apiResult
-//    }
-
     suspend fun searchPokemonShakespeareDescription(pokemonName: String) : PokemonApiResult<String>  {
         var response: PokemonApiResult<String>
         withContext(Dispatchers.IO) {
@@ -64,65 +32,29 @@ class PokemonClient internal constructor(
         return response
     }
 
-//        private val _pokemonSpriteSearchResult = MutableLiveData(PokemonApiResult<PokemonSprite>())
-//        val pokemonSpriteSearchResult: LiveData<PokemonApiResult<PokemonSprite>> = _pokemonSpriteSearchResult
-//
-//        fun searchPokemonSprite(pokemonName: String) : PokemonApiResult<PokemonSprite> {
-//            val apiResult = PokemonApiResult<PokemonSprite>()
-//            _pokemonSpriteSearchResult.value = apiResult
-//            CoroutineScope(Dispatchers.IO).launch {
-//                fetchPokemonDescription(
-//                    searchText = pokemonName,
-//                    onError = { apiError ->
-//                        this.launch(context = Dispatchers.Main) {
-//                            apiResult.apply {
-//                                resultState = ResultState.ERROR
-//                                error = apiError?.toPokemonError()
-//                            }
-//                            _pokemonSpriteSearchResult.value = apiResult
-//                        }
-//                    },
-//                    onSuccess = { pokemon ->
-//                        this.launch(context = Dispatchers.Main) {
-//                            apiResult.apply {
-//                                resultState = ResultState.SUCCESS
-//                                result = pokemon
-//                            }
-//                            _pokemonSpriteSearchResult.value = apiResult
-//                        }
-//                    },
-//                )
-//            }
-//            return apiResult
-//        }
+    /**
+     * @param sdkKey Unique identifier for anyone using this SDK. Just a placeholder for now.
+     * @param appContext Application Context of app using this sdk
+     */
+    class Builder(private val sdkKey: String = "SDK_KEY", private val appContext: Context) {
+        // Configurable options here
+        private var logLevel: PokemonClientLogLevel = PokemonClientLogLevel.DEBUG
 
+        fun logLevel(level: PokemonClientLogLevel) : Builder {
+            logLevel = level
+            return this
+        }
 
-
-
-
-        /**
-         * @param sdkKey Unique identifier for anyone using this SDK. Just a placeholder for now.
-         * @param appContext Application Context of app using this sdk
-         */
-        class Builder(private val sdkKey: String = "SDK_KEY", private val appContext: Context) {
-            // Configurable options here
-            private var logLevel: PokemonClientLogLevel = PokemonClientLogLevel.DEBUG
-
-            fun logLevel(level: PokemonClientLogLevel) : Builder {
-                logLevel = level
-                return this
-            }
-
-            fun build() : PokemonClient {
-                return buildPokemonClient().also {
-                    instance = it
-                }
-            }
-
-            private fun buildPokemonClient() : PokemonClient {
-                return PokemonClient(sdkKey, appContext, logLevel)
+        fun build() : PokemonClient {
+            return buildPokemonClient().also {
+                instance = it
             }
         }
+
+        private fun buildPokemonClient() : PokemonClient {
+            return PokemonClient(sdkKey, appContext, logLevel)
+        }
+    }
 
     companion object {
         private var instance: PokemonClient? = null
